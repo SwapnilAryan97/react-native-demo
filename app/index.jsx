6bca2bd6d221107877908a235c8f9fabe87d30c4
Swapnil Sinha <saiamthere@gmail.com>
2024-08-19 13:48:14 -0400

    remove css-in-js

--- app/index.jsx
+++ app/index.jsx
@@ -4,19 +4,10 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>First try at react native!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">First try at react native!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
